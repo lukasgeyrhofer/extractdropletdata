a263d724d8420fa35dcc8afc99f46e98df503691
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2017-12-02 23:49:10 +0200

    fit on log-sigmoid

--- millidrop_EstimateGrowthRate2Step.py
+++ millidrop_EstimateGrowthRate2Step.py
@@ -11,8 +11,18 @@ import millidrop_dataclass_new as mdc
 
 # sigmoid with minimum value.
 # a few parameters should be postive, thus takes their square here
-def sigmoid(t,center,width2,minval2,maxval2):
-    return minval2**2 + (maxval2**2 - minval2**2)/(1 + np.exp(-(t-center)/width2**2))
+# furthermore, fit on logarithm of sigmoid curve, this should put more emphasis on initial times
+def sigmoid(t,rate,centerpos,minval2,maxval2):
+    return np.log(minval2**2 + (maxval2**2 - minval2**2)/(1 + np.exp(- rate*(t-centerpos))))
+
+def extend(x,fulllength):
+    start = int((len(x)-fulllength)/2)
+    return np.concatenate([np.zeros(start),x,np.zeros(fulllength-len(x)-start)])
+
+def compute_weights(growthrates,numberpoints):
+    w = np.exp(-np.concatenate([np.ones(numberpoints/2)*growthrates[0],growthrates,np.ones(numberpoints/2)*growthrates[-1]]))
+    w /= np.sum(w)
+    return w
 
 parser = argparse.ArgumentParser()
 parser = mdc.AddCommandLineParameters(parser)
@@ -20,6 +30,12 @@ parser_gr = parser.add_argument_group(description = "==== Growthrate Estimation 
 parser_gr.add_argument("-n","--numberpoints",default=4,type=int)
 parser_gr.add_argument("-f","--upperfrac",default=.95,type=float)
 parser_gr.add_argument("-M","--maxfev",default=1000,type=int)
+parser_io = parser.add_argument_group(description = "==== I/O ====")
+#parser_io.add_argument("-o","--baseoutname",default="")
+parser_io.add_argument("-S","--skipsecond",default=False,action="store_true")
+parser_io.add_argument("-H","--compute_histogram",default=False,action="store_true")
+parser_io.add_argument("-R","--histo_range",default=[0,2],nargs=2,type=float)
+parser_io.add_argument("-B","--histo_bins",default=40,type=int)
 args = parser.parse_args()
 
 data = mdc.DropletDataNew(**vars(args))
@@ -27,8 +43,8 @@ data.ExcludeLabel('Empty')
 
 
 for label in data.labels:
-    grall = list()
-    grall2step = list()
+    gr_all = list()
+    gr_all_2step = list()
     for dropdata in data[label]:
         # dropdata contains the whole pandas object loaded from the csv-file
         # separate into back/forth, choose channel of signal, rescale time
@@ -40,41 +56,48 @@ for label in data.labels:
         gr1 = np.array([mdc.MLSQ_fit(t1[i:i+args.numberpoints,0],np.log(t1[i:i+args.numberpoints,1]),returnoffset = False) for i in range(len(t1[:,0])-args.numberpoints)],dtype=np.float)
         gr2 = np.array([mdc.MLSQ_fit(t2[i:i+args.numberpoints,0],np.log(t2[i:i+args.numberpoints,1]),returnoffset = False) for i in range(len(t2[:,0])-args.numberpoints)],dtype=np.float)
 
-        # add to list
-        grall.append(mdc.MeanUpperFrac(gr1,args.upperfrac))
-        grall.append(mdc.MeanUpperFrac(gr2,args.upperfrac))
+        np.savetxt(args.outbasename + '{:04d}-{}F'.format(i,label),np.transpose([t1[:,0],t1[:,1]]))
+        np.savetxt(args.outbasename + '{:04d}-{}B'.format(i,label),np.transpose([t2[:,0],t2[:,1]]))
+        
 
-
-        # second step:
-        # use growthrates as inverse weights to fit global sigmoid
-        
-        # estimate start values for fit
-        # a few values need to be restricted to positive values, thus they are squared in the function above, need to take root here
-        param0 = np.array([(t1[:,0])[((t1[:,1]-t1[0,1])*(t1[-1,1]-t1[:,1])).argmax()],1,np.sqrt(t1[0,1]),np.sqrt(t1[-1,1])])
-        
-        # define weights as inverse growthrates
-        # rescale properly first, and add a small number to avoid dividing by 0
-        weights1 = 1./(gr1 - np.min(gr1) + 1e-10)
-        weights2 = 1./(gr2 - np.min(gr2) + 1e-10)
-        startindex = int(args.numberpoints/2)
-        
-        # fit sigmoids
-        try:
-            fit1,cov1 = curve_fit(sigmoid,t1[startindex:len(t1[:,0])-startindex,0],t1[startindex:len(t1[:,0])-startindex,1],p0 = param0,sigma = weights1,maxfev = args.maxfev)
-            grall2step.append(1./fit1[0]**2)
-        except:
-            continue
-        
-        try:
-            fit2,cov2 = curve_fit(sigmoid,t2[startindex:len(t2[:,0])-startindex,0],t2[startindex:len(t2[:,0])-startindex,1],p0 = param0,sigma = weights2,maxfev = args.maxfev)
-            grall2step.append(1./fit2[0]**2)
-        except:
-            continue
-        
         # add to list
-        
-        #fp = open(label + '_growthrates','w')
-    np.savetxt(label + '_growthrates',grall)
-    np.savetxt(label + '_growthrates_2step',grall2step)
-    #print grall
-    #print grall2step
+        gr_all.append(mdc.MeanUpperFrac(gr1,args.upperfrac))
+        gr_all.append(mdc.MeanUpperFrac(gr2,args.upperfrac))
+
+        if not args.skipsecond:
+            # second step:
+            # use growthrates as inverse weights to fit global sigmoid
+            
+            # estimate start values for fit
+            # a few values need to be restricted to positive values, thus they are squared in the function above, need to take root here
+            param0 = np.array([(t1[:,0])[((t1[:,1]-t1[0,1])*(t1[-1,1]-t1[:,1])).argmax()],1,np.sqrt(t1[0,1]),np.sqrt(t1[-1,1])])
+            
+            # define weights as inverse growthrates
+            weights1 = compute_weights(gr1,args.numberpoints)
+            weights2 = compute_weights(gr2,args.numberpoints)
+            
+            # fit sigmoids
+            try:
+                fit1,cov1 = curve_fit(sigmoid,t1[:,0],np.log(t1[:,1]),p0 = param0,sigma = weights1,maxfev = args.maxfev)
+                gr_all_2step.append(fit1[0])
+            except:
+                pass
+            
+            try:
+                fit1,cov1 = curve_fit(sigmoid,t1[:,0],np.log(t1[:,1]),p0 = param0,sigma = weights1,maxfev = args.maxfev)
+                gr_all_2step.append(fit1[0])
+            except:
+                pass
+            
+    np.savetxt(args.outbasename + label + '_growthrates',gr_all)
+    if args.compute_histogram:
+        h,b = np.histogram(gr_all,range = args.histo_range,bins = histo_bins)
+        b = .5*(b[1:] + b[:-1])
+        np.savetxt(args.outbasename + label + '_histogram',np.transpose([b,h]))
+    
+    if not args.skipsecond:
+        np.savetxt(args.outbasename + label + '_growthrates_2step',gr_all_2step)
+        if args.compute_histogram:
+            h,b = np.histogram(gr_all_2step,range = args.histo_range,bins = histo_bins)
+            b = .5*(b[1:] + b[:-1])
+            np.savetxt(args.outbasename + label + '_histogram_2step',np.transpose([b,h]))
